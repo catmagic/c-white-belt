ec1cbf9220d822dbcf0059715807c647c81f0a4a
Cheshire Cat <catmagic000@gmail.com>
2023-06-17 10:34:45 +0000

    square equation

---
+++ 1weekask/1.5.2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cmath>
+int main()
+{
+	int a,b,c;
+	std::cin>>a>>b>>c;
+	if(a==0)
+	{
+		if(b!=0)
+		{
+			std::cout<<-c/b;	
+		}
+	}
+	else
+	{
+		if(b*b>4*a*c)
+		{
+			std::cout<<(-b-sqrt(b*b-4*a*c))/2/a<<" "<<(-b+sqrt(b*b-4*a*c))/2/a<<std::endl;
+		}
+		if(b*b==4*a*c)
+		{
+			std::cout<<-b/2/a;
+		}
+	}
+	return 0;
+}
