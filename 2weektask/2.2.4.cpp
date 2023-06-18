03373c62440427e4c21d3b3986320e3d8aaa226f
Cheshire Cat <catmagic000@gmail.com>
2023-06-18 09:40:47 +0000

    reverse 2

---
+++ 2weektask/2.2.4.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+std::vector<int> Reversed(std::vector<int>& v)
+{
+	std::vector<int> res(v.size());
+	for(int i=0;i<v.size();++i)
+	{
+		res[v.size()-i-1]=v[i];
+	}
+	return res;
+}
+/*int main()
+{
+	std::vector<int> numbers = {1, 5, 3, 4, 2};
+	std::vector<int> res;
+	res=Reversed(numbers);
+	for(auto number : res)
+	{
+		std::cout<<number<<" ";
+	}
+	return 0;
+}*/
