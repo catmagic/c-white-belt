d624464bd568df567497f5a221896a86ccb4fd66
Cheshire Cat <catmagic000@gmail.com>
2023-06-18 09:45:02 +0000

    temp

--- 2weektask/2.2.4.cpp
+++ 2weektask/2.2.4.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-std::vector<int> Reversed(const std::vector<int>& v)
+vector<int> Reversed(const vector<int>& v)
 {
 	std::vector<int> res(v.size());
 	for(int i=0;i<v.size();++i)
