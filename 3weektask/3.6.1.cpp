ee91899705d87232398b1c63208b4b88c8052d1f
Cheshire Cat <catmagic000@gmail.com>
2023-06-22 13:20:58 +0000

    string

---
+++ 3weektask/3.6.1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class ReversibleString
+{
+public:
+	ReversibleString(string s="")
+	{
+		this->s=s;
+	}
+	void Reverse()
+	{
+		s=string(s.rbegin(),s.rend());
+	}
+	string ToString()const 
+	{
+		return s;
+	}
+private:
+string s;
+};
+int main() {
+  ReversibleString s("live");
+  s.Reverse();
+  cout << s.ToString() << endl;
+  
+  s.Reverse();
+  const ReversibleString& s_ref = s;
+  string tmp = s_ref.ToString();
+  cout << tmp << endl;
+  
+  ReversibleString empty;
+  cout << '"' << empty.ToString() << '"' << endl;
+  
+  return 0;
+}
