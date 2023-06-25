9d20bf46dd091387f6213939a30c94c495e78e2c
Сat Cheshire <catmagic000@gmail.com>
2023-06-25 12:09:20 +0300

    means inicialization

---
+++ 3weektask/3.6.3.cpp
@@ -0,0 +1,18 @@
+class Incognizable
+{
+    public:
+    Incognizable(int a=0,int b=0)
+    {
+        this->a=a;
+        this->b=b;
+     }
+ private:
+    int a,b;
+};
+/*int main() {
+  Incognizable a;
+  Incognizable b = {};
+  Incognizable c = {0};
+  Incognizable d = {0, 1};
+  return 0;
+}*/
